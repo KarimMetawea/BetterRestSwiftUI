03258ddd00457f4279415850f0cd93fb701feb90
Karim Metawea <ks.metawea@mohre.gov.ae>
2021-09-19 14:33:41 +0400

    Initial Commit

---
+++ BetterRestApp/BetterRestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestAppApp.swift
+//  BetterRestApp
+//
+//  Created by Karem on 9/19/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
